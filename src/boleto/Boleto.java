9d577d282c93a5c7db6a54eafa40d3d9fb356520
Lucas <lucasrdsf@id.uff.br>
2020-10-05 14:21:21 -0300

    Implementando Teste de pagamento

---
+++ src/boleto/Boleto.java
@@ -0,0 +1,5 @@
+package boleto;
+
+class Boleto {
+
+}
---
+++ test/pagamento/PagamentoFaturaTest.java
@@ -0,0 +1,14 @@
+package pagamento;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PagamentoFaturaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
